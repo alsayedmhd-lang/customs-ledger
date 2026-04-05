5c7ab115f813c4d1fea78c23b831bac7cb2c5fda
alsayedmhd-lang <alsayed.mhd@gmail.com>
2026-04-05 20:27:48 +0300

    Update app.ts

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express"; // أضفنا Request و Response للوضوح
 import cors from "cors";
 import router from "./routes";
 
@@ -7,6 +7,12 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// *********** أضف هذا الجزء هنا ***********
+app.get("/", (req: Request, res: Response) => {
+  res.send("🚀 Customs Ledger API is running smoothly!");
+});
+// *****************************************
 
 app.use("/api", router);
 
