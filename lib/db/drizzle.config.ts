2ddfc6b116e0595fcce523c63cbc1b0575022406
alsayedmhd-lang <alsayed.mhd@gmail.com>
2026-04-07 22:07:14 +0300

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,14 +1,32 @@
 import { defineConfig } from "drizzle-kit";
 import path from "path";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
 export default defineConfig({
   schema: path.join(__dirname, "./src/schema/index.ts"),
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    host: process.env.PGHOST!,
+    port: Number(process.env.PGPORT || 5432),
+    user: process.env.PGUSER!,
+    password: process.env.PGPASSWORD!,
+    database: process.env.PGDATABASE || "postgres",
+    ssl: "require",
   },
 });
+
+
+
+// import { defineConfig } from "drizzle-kit";
+// import path from "path";
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL, ensure the database is provisioned");
+// }
+
+// export default defineConfig({
+//   schema: path.join(__dirname, "./src/schema/index.ts"),
+//   dialect: "postgresql",
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL,
+//   },
+// });
