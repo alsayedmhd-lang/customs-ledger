d17f6b0ddb1a01a3dfc4709cb7c8c0fae0135414
alsayedmhd-lang <alsayed.mhd@gmail.com>
2026-04-10 16:17:31 +0300

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,39 +1,17 @@
+
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema";
 
 const { Pool } = pg;
 
-export const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT || 5432),
-  database: process.env.PGDATABASE || "postgres",
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  ssl: { rejectUnauthorized: false },
-});
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
+}
 
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
 export * from "./schema";
-
-
-
-
-
-// import { drizzle } from "drizzle-orm/node-postgres";
-// import pg from "pg";
-// import * as schema from "./schema";
-
-// const { Pool } = pg;
-
-// if (!process.env.DATABASE_URL) {
-//   throw new Error(
-//     "DATABASE_URL must be set. Did you forget to provision a database?",
-//   );
-// }
-
-// export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-// export const db = drizzle(pool, { schema });
-
-// export * from "./schema";
