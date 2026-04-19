e86f9f669bd60c83d671cfb2f109d5dc391d3235
alsayedmhd-lang <alsayed.mhd@gmail.com>
2026-04-19 20:57:28 +0300

    Change layout from horizontal to vertical for invoice form

--- customs-accounting/src/pages/invoices/form.tsx
+++ customs-accounting/src/pages/invoices/form.tsx
@@ -386,7 +386,7 @@ export default function InvoiceForm() {
       dir={isRTL ? "rtl" : "ltr"}
       className="max-w-4xl mx-auto space-y-4 pb-24"
     >
-      <div className="flex items-start justify-between gap-3 w-full">
+      <div className="flex flex-col gap-3 w-full">
         <div className="flex items-center gap-3 order-2">
           <button
             onClick={() => setLocation("/invoices")}
